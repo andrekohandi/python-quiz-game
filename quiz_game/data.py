d148869e58b877f6b70793b8c3fff0f31f8db30a
Andre Panji <54766764+andrekohandi@users.noreply.github.com>
2021-10-16 20:52:16 +0700

    Add files via upload

---
+++ quiz_game/data.py
@@ -0,0 +1,119 @@
+question_data = [
+    {
+        "category":"General Knowledge",
+        "type":"boolean","difficulty":"medium",
+        "question":"'Typewriter' is the longest word that can be typed using only the first row on a QWERTY keyboard.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"Coca-Cola's original colour was green.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The US emergency hotline is 911 because of the September 11th terrorist attacks.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The vapor produced by e-cigarettes is actually water.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The sum of all the numbers on a roulette wheel is 666.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"Furby was released in 1998.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The bikini is named after the 'Bikini Atoll', an island where the United States conducted tests on atomic bombs.","correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"Cucumbers are usually more than 90% water.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"You are allowed to sell your soul on eBay.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The term 'Spam' came before the food product 'Spam'.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"Sitting for more than three hours a day can cut two years off a person's life expectancy.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"The French word to travel is 'Travail'.",
+        "correct_answer":"False",
+        "incorrect_answers":["True"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"Fast food restaurant chains Carl's Jr. and Hardee's are owned by the same company.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean","difficulty":"medium",
+        "question":"The scientific name for the Southern Lights is Aurora Australis",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    },
+    {
+        "category":"General Knowledge",
+        "type":"boolean",
+        "difficulty":"medium",
+        "question":"Haggis is traditionally ate on Burns Night.",
+        "correct_answer":"True",
+        "incorrect_answers":["False"]
+    }
+]
